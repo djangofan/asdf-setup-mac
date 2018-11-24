8f95309263053a5cd82a5247a29b13a6454fe0fa
Jon Austen <jausten@MACPRO.domain>
2018-11-24 13:28:01 -0800

    Initial commit.

---
+++ setupASDF.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+DIRECTORY=$HOME/.asdf
+
+if [[ ! -d "$DIRECTORY" ]]; then
+  brew install openssl
+  brew install gpg
+  brew install gnupg
+  brew install coreutils
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.1
+  echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bash_profile
+  echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.bash_profile
+fi
+
+# java
+asdf plugin-add java
+#asdf list-all java
+asdf install java oracle-8.191
+asdf global java oracle-8.191
+java -version
+
+# nodejs https://github.com/asdf-vm/asdf-nodejs
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+#asdf list-all nodejs
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+asdf install nodejs 11.2.0
+asdf global nodejs 11.2.0
+npm -v
+
+# maven
+asdf plugin-add maven
+#asdf list-all maven
+asdf install maven 3.6.0
+asdf global maven 3.6.0
+mvn -v
+
+# gradle 
+asdf plugin-add gradle
+#asdf list-all gradle
+asdf install gradle 4.9
+asdf global gradle 4.9
+gradle -v
+
+# ruby
+asdf plugin-add ruby
+#asdf list-all ruby
+asdf install ruby 2.5.3
+asdf global ruby 2.5.3
+ruby -v
+
+# python
+asdf plugin-add python https://github.com/tuvistavie/asdf-python.git
+#asdf list-all python
+asdf install python 3.7.1
+asdf global python 3.7.1
+python -v
